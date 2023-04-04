e26dc6a438cec900207cb9d323cfd70fccb0e9f1
Ricardo Alonzo Ugalde <ricardo.alonzo.ugalde@gmail.com>
2023-04-03 21:29:45 -0700

    now with the ability to select a story genre!

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request
 import openai
+import os
 
-openai.api_key = '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'
+openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 app = Flask(__name__)
 
@@ -12,11 +13,20 @@ def index():
 @app.route('/process_input', methods=['POST'])
 def process_input():
     user_input = request.form['user_input']
-    story = generate_story(user_input)  # Call the generate_story function
-    return render_template('story.html', story=story)
+    story_type = request.form['story_type']
+    # process the form data and generate the story
+    title, story = generate_story(story_type, user_input)
 
-def generate_story(user_input):
-    prompt = f"Create a short story based on the user's input: {user_input}"
+    return render_template('story.html', title=title, story=story, user_input=user_input)
+
+
+def generate_story(story_type, user_input):
+    if story_type == 'sad':
+        prompt = f"Write a sad story based on the user's input: {user_input}"
+    elif story_type == 'funny':
+        prompt = f"Write a funny story based on the user's input: {user_input}"
+    else:
+        prompt = f"Write a horror story based on the user's input: {user_input}"
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
@@ -28,7 +38,6 @@ def generate_story(user_input):
     story = response.choices[0].text.strip()
     title = response.choices[0].text.strip().split('.')[0]  # Extract first sentence as title
     return title, story
-
 
 if __name__ == '__main__':
     app.run(debug=True)
